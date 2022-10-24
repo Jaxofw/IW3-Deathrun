3b38f223d0cff7800b4c95c07528f70401a04650
actualmist <jamie.hillhouse@protonmail.com>
2022-10-24 20:48:19 +0100

    added weapons

--- runMod.bat
+++ runMod.bat
@@ -1,2 +1,2 @@
 cd ..\..\
-iw3mp.exe +set fs_game "mods\arcane_deathrun_dev" +g_gametype deathrun +set developer 2 +set developer_script 1 +exec deathrun.cfg +devmap mp_deathrun_waterworld
+iw3mp.exe +set fs_game "mods\arcane_deathrun_dev" +g_gametype deathrun +set developer 0 +set developer_script 1 +exec deathrun.cfg +devmap mp_deathrun_waterworld
