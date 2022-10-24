20330be391cea5ea052bce3070f85700403d93be
JaxOFW <jaxofw@gmail.com>
2022-10-24 03:04:41 -0400

    Init Rank Table
    https://trello.com/c/bYdthLey/16-levels

--- makeMod.bat
+++ makeMod.bat
@@ -5,6 +5,7 @@ xcopy images ..\..\raw\images /SY
 xcopy maps ..\..\raw\maps /SY
 xcopy material_properties ..\..\raw\material_properties /SY
 xcopy materials ..\..\raw\materials /SY
+xcopy mp ..\..\raw\mp /SY
 xcopy ui_mp ..\..\raw\ui_mp /SY
 
 copy /Y mod.csv ..\..\zone_source
