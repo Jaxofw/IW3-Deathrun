520e98028d2aa94b6b63dae2d61a8f24a480f7fc
JaxOFW <jaxofw@gmail.com>
2022-11-29 19:41:53 -0500

    Cleaned up bat files and edited gitignore

--- main.bat
+++ main.bat
@@ -1,5 +1,24 @@
-del mod.ff
+set launchOptions="+set fs_game "mods\arcane_deathrun_dev" +g_gametype deathrun" +exec server.cfg +map mp_dr_wtf
 
+@ECHO OFF
+CLS
+ECHO 1. Build Mod
+ECHO 2. Build IWDs
+ECHO 3. Run Mod
+ECHO 4. Run Server
+ECHO 5. Exit
+ECHO.
+
+CHOICE /C 12345 /M "Enter your choice: "
+
+IF ERRORLEVEL 5 GOTO CloseAll
+IF ERRORLEVEL 4 GOTO RunServer
+IF ERRORLEVEL 3 GOTO RunMod
+IF ERRORLEVEL 2 GOTO BUILD_IWDS
+IF ERRORLEVEL 1 GOTO BUILD_MOD
+
+:BUILD_MOD
+del mod.ff
 xcopy animtrees ..\..\raw\animtrees /SY
 xcopy braxi ..\..\raw\braxi /SY
 xcopy english ..\..\raw\english /SY
@@ -18,12 +37,32 @@ xcopy xanim ..\..\raw\xanim /SY
 xcopy xmodel ..\..\raw\xmodel /SY
 xcopy xmodelparts ..\..\raw\xmodelparts /SY
 xcopy xmodelsurfs ..\..\raw\xmodelsurfs /SY
-
 copy /Y mod.csv ..\..\zone_source
 
 cd ..\..\bin
 linker_pc.exe -language english -compress -cleanup mod
 cd ..\mods\arcane_deathrun_dev
 copy ..\..\zone\english\mod.ff
+GOTO End
 
-pause
+:BUILD_IWDS
+del arcane.iwd
+7za a -r -tzip arcane.iwd images/
+7za a -r -tzip arcane.iwd weapons/
+7za a -r -tzip arcane.iwd sound/
+GOTO End
+
+:runMod
+pushd ..\..\
+iw3mp.exe %launchOptions% +set developer 1 +set developer_script 0
+GOTO End
+
+:RunServer
+pushd ..\..\
+cod4x18_dedrun.exe %launchOptions% +set dedicated 1 +set net_port 28961
+GOTO End
+
+:CloseAll
+exit
+
+:End
--- makeIWD.bat
+++
@@ -1,7 +0,0 @@
-del arcane.iwd
-
-7za a -r -tzip arcane.iwd images/
-7za a -r -tzip arcane.iwd weapons/
-7za a -r -tzip arcane.iwd sound/
-
-pause
